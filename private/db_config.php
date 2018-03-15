c4c96a896754280da16aea0c26f9947156cef996
romanhenry <roman.henry@bobcats.gcsu.edu>
2018-03-15 12:04:46 -0400

    changed username and db name to new romanh03_pro3 and romanh03_retirement

--- private/db_config.php
+++ private/db_config.php
@@ -4,8 +4,8 @@
 // you need to change these values to match your Database
 // these values need a ""
 define("DB_SERVER", "localhost");
-define("DB_USER", "romanh03_pro1");
-define("DB_PASS", "Pwsxz9DqR05");
-define("DB_NAME", "romanh03_pro1");
+define("DB_USER", "romanh03_pro3");
+define("DB_PASS", "gobraves");
+define("DB_NAME", "romanh03_retirement");
 
 ?>
